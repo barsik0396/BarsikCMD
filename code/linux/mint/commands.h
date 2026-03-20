1cfbd0e2ccd2073ac400b4a872b619cf0274b3ba
barsik0396 <Barsik0396@outlook.com>
2026-03-20 18:14:39 +0200

    Обновил хедер команд

--- code/linux/mint/commands.h
+++ code/linux/mint/commands.h
@@ -4,10 +4,15 @@
 #include <map>
 #include <functional>
 
+#include "commands/meow.h"
+#include "commands/update.h"
+
 void cmd_help(const std::map<std::string, std::function<void(const std::vector<std::string>&)>>& commands,
               const std::vector<std::string>& args);
 void cmd_echo(const std::vector<std::string>& args);
 void cmd_clear(const std::vector<std::string>& args);
 void cmd_ver(const std::vector<std::string>& args);
 void cmd_exit(const std::vector<std::string>& args);
-void cmd_update(const std::vector<std::string>& args);
+void cmd_meow(const std::vector<std::string>& args);
+void cmd_info(const std::vector<std::string>& args);
+void cmd_reload(const std::vector<std::string>& args);
